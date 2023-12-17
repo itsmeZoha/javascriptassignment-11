b6fc7c298f34e330db30f4641cab4ca05bb0aefe
Zoha Asghar <callmezoiii42@gmail.com>
2023-12-18 00:45:38 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let userName = prompt("Please Enter your name to visit our website? ") 
+// window.onload = function(){
+//    let message ="Good Day"
+// //    let firstName = prompt("Please Enter Your First Name?")
+// //    let lastName = prompt("Please Enter Your Last Name?")
+// //    let fullName = firstName +" "+ lastName
+//    let banger ="!"
+//    document.getElementById("userName").innerHTML = message + " " + userName+ banger
+// }
+//---------------------if else Statement---------------------//
+function makeInvisible(){
+   document.getElementById('apple').className = "hidden"
+}
+function makeBig(){
+   document.getElementById('title').className += " big"
+}
